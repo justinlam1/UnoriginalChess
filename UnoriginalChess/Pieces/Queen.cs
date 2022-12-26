3ac667790b44c5d3d64676b26d187963e1f8898a
Justin <justin.lam640@gmail.com>
2022-12-25 22:04:22 -0500

    Added logic for calculating all legal moves for Queen pieces

--- UnoriginalChess/Pieces/Queen.cs
+++ UnoriginalChess/Pieces/Queen.cs
@@ -8,6 +8,52 @@ internal class Queen : Piece
 
     public override List<Move> GetLegalMoves(Board board)
     {
-        throw new NotImplementedException();
+        var moves = new List<Move>();
+
+        // Check moves in each direction
+        moves.AddRange(GetLegalMovesInDirection(board, 1, 0));
+        moves.AddRange(GetLegalMovesInDirection(board, 1, -1));
+        moves.AddRange(GetLegalMovesInDirection(board, 0, -1));
+        moves.AddRange(GetLegalMovesInDirection(board, -1, -1));
+        moves.AddRange(GetLegalMovesInDirection(board, -1, 0));
+        moves.AddRange(GetLegalMovesInDirection(board, -1, -1));
+        moves.AddRange(GetLegalMovesInDirection(board, 0, 1));
+        moves.AddRange(GetLegalMovesInDirection(board, 1, 1));
+
+        return moves;
+    }
+
+    private IEnumerable<Move> GetLegalMovesInDirection(Board board, int rowDelta, int columnDelta)
+    {
+        var moves = new List<Move>();
+
+        var nextRow = Row + rowDelta;
+        var nextColumn = Column + columnDelta;
+
+        while (nextRow >= 0 && nextRow < board.BoardRows && nextColumn >= 0 &&
+               nextColumn < board.BoardColumns)
+        {
+            if (board.Cells[nextRow][nextColumn].Piece == null)
+            {
+                // If cell is empty, add the move to the list
+                moves.Add(new Move(new Position(Row, Column), new Position(nextRow, nextColumn)));
+            }
+            else if (board.Cells[nextRow][nextColumn].Piece.Color != this.Color)
+            {
+                // If the cell contains a piece of the opposite color, add the move to the list then stop
+                moves.Add(new Move(new Position(Row, Column), new Position(nextRow, nextColumn)));
+                break;
+            }
+            else
+            {
+                // If the cell contains a piece of the same color, then stop
+                break;
+            }
+            
+            nextRow = Row + rowDelta;
+            nextColumn = Column + columnDelta;
+        }
+
+        return moves;
     }
 }
