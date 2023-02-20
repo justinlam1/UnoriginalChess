ec702540dde3b2e10964e0c3e6a1fff24a52a5d6
Justin <justin.lam640@gmail.com>
2023-02-20 00:32:23 -0500

    Implemented basic bishop movement

---
+++ UnoriginalChess.UnitTests/BishopTests.cs
@@ -0,0 +1,55 @@
+using UnoriginalChess.Pieces;
+
+namespace UnoriginalChess.UnitTests;
+
+public class BishopTests
+{
+    private Board board;
+
+    public BishopTests()
+    {
+        board = new Board(8, 8);
+    }
+    
+    [Fact]
+    public void Bishop_Can_Move_In_All_Directions()
+    {
+        // Arrange
+        var bishop = new Bishop(PlayerColor.White, 3, 3);
+        board.Cells[3][3].Piece = bishop;
+
+        // Act
+        var legalMoves = bishop.GetLegalMoves(board);
+
+        // Assert
+        Assert.Contains(new Move(new Position(3, 3), new Position(4, 4)), legalMoves);
+        Assert.Contains(new Move(new Position(3, 3), new Position(4, 2)), legalMoves);
+        Assert.Contains(new Move(new Position(3, 3), new Position(2, 2)), legalMoves);
+        Assert.Contains(new Move(new Position(3, 3), new Position(2, 4)), legalMoves);
+    }
+    
+    [Fact]
+    public void Bishop_Cannot_Move_Through_Friendly_Pieces()
+    {
+        var bishop = new Bishop(PlayerColor.White, 3, 3);
+        var pawn = new Pawn(PlayerColor.White, 4, 4);
+        board.Cells[3][3].Piece = bishop;
+        board.Cells[4][4].Piece = pawn;
+
+        var legalMoves = bishop.GetLegalMoves(board);
+        
+        Assert.DoesNotContain(new Move(new Position(3, 3), new Position(4, 4)), legalMoves);
+    }
+
+    [Fact]
+    public void Bishop_Cannot_Move_Out_Of_Bounds()
+    {
+        var bishop = new Bishop(PlayerColor.White, 3, 0);
+        board.Cells[3][0].Piece = bishop;
+        
+        var legalMoves = bishop.GetLegalMoves(board);
+        
+        Assert.DoesNotContain(new Move(new Position(3, 0), new Position(4, -1)), legalMoves);
+        Assert.DoesNotContain(new Move(new Position(3, 0), new Position(2, -1)), legalMoves);
+    }
+}
--- UnoriginalChess/Pieces/Bishop.cs
+++ UnoriginalChess/Pieces/Bishop.cs
@@ -8,6 +8,50 @@ internal class Bishop : Piece
 
     public override List<Move> GetLegalMoves(Board board)
     {
-        throw new NotImplementedException();
+        var moves = new List<Move>();
+
+        // Check moves in each direction
+        moves.AddRange(GetLegalMovesInDirection(board, 1, -1));
+        moves.AddRange(GetLegalMovesInDirection(board, -1, -1));
+        moves.AddRange(GetLegalMovesInDirection(board, -1, 1));
+        moves.AddRange(GetLegalMovesInDirection(board, 1, 1));
+
+        return moves;
+    }
+
+    private IEnumerable<Move> GetLegalMovesInDirection(Board board, int rowDelta, int columnDelta)
+    {
+        var moves = new List<Move>();
+
+        var nextRow = Row + rowDelta;
+        var nextColumn = Column + columnDelta;
+
+        while (nextRow >= 0 && nextRow < board.BoardRows && nextColumn >= 0 &&
+               nextColumn < board.BoardColumns)
+        {
+            var pieceAtDestination = board.Cells[nextRow][nextColumn].Piece;
+            
+            if (pieceAtDestination == null)
+            {
+                // If cell is empty, add the move to the list
+                moves.Add(new Move(new Position(Row, Column), new Position(nextRow, nextColumn)));
+            }
+            else if (pieceAtDestination.Color != Color)
+            {
+                // If the cell contains a piece of the opposite color, add the move to the list then stop
+                moves.Add(new Move(new Position(Row, Column), new Position(nextRow, nextColumn)));
+                break;
+            }
+            else
+            {
+                // If the cell contains a piece of the same color, then stop
+                break;
+            }
+            
+            nextRow += rowDelta;
+            nextColumn += columnDelta;
+        }
+
+        return moves;
     }
 }
